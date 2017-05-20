b6b21153fe67e5c8fec2209484ce04bee40ecf62
Tejas Manohar <github@tejas.io>
2017-05-19 22:00:08 -0500

    feat: support vendor (#7)
    prevents errors like
    package github.com/segmentio/db-service/client
    	imports github.com/segmentio/db-service/vendor/gopkg.in/mgo.v2/bson: must be imported as gopkg.in/mgo.v2/bson

--- provider/internal/types.go
+++ provider/internal/types.go
@@ -3,7 +3,9 @@ package internal
 import (
 	"fmt"
 	"go/types"
+	"path/filepath"
 	"reflect"
+	"strings"
 
 	"github.com/tejasmanohar/glue/provider"
 )
@@ -47,28 +49,30 @@ func Unpack(t types.Type) types.Type {
 }
 
 func GetTypeInfo(t types.Type) provider.TypeInfo {
+	var ret provider.TypeInfo
+
 	switch specific := t.(type) {
 	case *types.Pointer:
-		return GetTypeInfo(specific.Elem())
+		ret = GetTypeInfo(specific.Elem())
 	case *types.Named:
 		obj := specific.Obj()
-		return provider.TypeInfo{
+		ret = provider.TypeInfo{
 			Identifier: fmt.Sprintf("%s.%s", obj.Pkg().Name(), obj.Name()),
 			Imports:    []string{obj.Pkg().Path()},
 		}
 	case *types.Basic:
-		return provider.TypeInfo{
+		ret = provider.TypeInfo{
 			Identifier: specific.Name(),
 		}
 	case *types.Slice:
 		underlying := GetTypeInfo(specific.Elem())
-		return provider.TypeInfo{
+		ret = provider.TypeInfo{
 			Identifier: fmt.Sprintf("[]%s", underlying.Identifier),
 			Imports:    underlying.Imports,
 		}
 	case *types.Array:
 		underlying := GetTypeInfo(specific.Elem())
-		return provider.TypeInfo{
+		ret = provider.TypeInfo{
 			Identifier: fmt.Sprintf("[]%s", underlying.Identifier),
 			Imports:    underlying.Imports,
 		}
@@ -76,11 +80,37 @@ func GetTypeInfo(t types.Type) provider.TypeInfo {
 		key := GetTypeInfo(specific.Key())
 		val := GetTypeInfo(specific.Elem())
 
-		return provider.TypeInfo{
+		ret = provider.TypeInfo{
 			Identifier: fmt.Sprintf("map[%s]%s", key.Identifier, val.Identifier),
 			Imports:    append(key.Imports, val.Imports...),
 		}
 	default:
 		panic(fmt.Errorf("unexpected type: %s", reflect.TypeOf(t)))
 	}
+
+	for i, pkg := range ret.Imports {
+		ret.Imports[i] = stripVendor(pkg)
+	}
+
+	return ret
+}
+
+// github.com/x/y/vendor/github.com/a/b -> github.com/a/b
+func stripVendor(path string) string {
+	dirs := strings.Split(path, string(filepath.Separator))
+	num := len(dirs)
+	vendorIndex := -1
+	for i := 1; i <= num; i++ {
+		dir := dirs[num-i]
+		if dir == "vendor" {
+			vendorIndex = num - i
+			break
+		}
+	}
+
+	if vendorIndex == -1 {
+		return path
+	}
+
+	return filepath.Join(dirs[vendorIndex+1:]...)
 }
